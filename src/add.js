0a01909486f9cbc0ec9a9ad869d17005409ff76b
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2025-03-16 21:09:58 +0530

    refactor add function

--- src/add.js
+++ src/add.js
@@ -1,38 +1,42 @@
 function add(expression) {
-    const delimiter = new Set([",", "\n"]);
-    let nums = expression;
-    if (expression.startsWith("//")) {
-        const [customDelimiter, ...numbers] = expression.split("\n");
-        delimiter.add(customDelimiter.at(-1));
-        nums = numbers.join("\n");
-    }
+  const delimiter = new Set([",", "\n"]);
+  let nums = expression;
 
-    let sum = 0;
-    let currentNumber = "";
-    const negativeNums = [];
-    for (let i = 0; i < nums.length; i++) {
-      if (delimiter.has(nums[i])) {
-        const parsedNum = parseInt(currentNumber || 0);
-        if (parsedNum < 0) {
-          negativeNums.push(parsedNum);
-        } else {
-          sum += parsedNum;
-        }
-        currentNumber = "";
-      } else {
-        currentNumber += nums[i];
-      }
-    }
-    parsedNum = parseInt(currentNumber || 0);
+  if (expression.startsWith("//")) {
+    const [customDelimiter, ...numbers] = expression.split("\n");
+    delimiter.add(customDelimiter.at(-1));
+    nums = numbers.join("\n");
+  }
+
+  let sum = 0;
+  let currentNumber = "";
+  const negativeNums = [];
+
+  function handleNegativeNums(num) {
+    const parsedNum = parseInt(num || 0);
     if (parsedNum < 0) {
       negativeNums.push(parsedNum);
     } else {
       sum += parsedNum;
     }
-    if (negativeNums.length > 0) {
-      throw new Error(`negative numbers not allowed ${negativeNums.join(",")}`);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (delimiter.has(nums[i])) {
+      handleNegativeNums(currentNumber);
+      currentNumber = "";
+    } else {
+      currentNumber += nums[i];
     }
-    return sum;
+  }
+
+  handleNegativeNums(currentNumber);
+
+  if (negativeNums.length > 0) {
+    throw new Error(`negative numbers not allowed ${negativeNums.join(",")}`);
+  }
+
+  return sum;
 }
 
 module.exports = add;
