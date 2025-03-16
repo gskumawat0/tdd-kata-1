37680173bbe5a6a8ac0c97364dcc2dd2716eacd0
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2025-03-16 20:27:14 +0530

    add test for dynamic delimiter

--- tests/add.test.js
+++ tests/add.test.js
@@ -16,4 +16,16 @@ describe("add", () => {
   it("should sum numbers with '\\n' delimiter", () => {
     expect(add("7\n2")).toBe(9);
   });
+
+  it("should sum n numbers with '\\n' delimiter", () => {
+    expect(add("7\n2\n5\n8")).toBe(22);
+  });
+
+  it("should sum n numbers with '\\n' and ',' delimiter", () => {
+    expect(add("7\n2,5\n8")).toBe(22);
+  });
+
+  it("should sum n numbers with dynamic delimiter", () => {
+    expect(add("\\;\n1;2")).toBe(3);
+  });
 });
